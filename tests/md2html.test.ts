83e0a3d56d74b3d49892607cc747287342d9ff31
Pistelja <pistelja@gmail.com>
2026-08-28 20:04:50 +0200

    Add R-H-01 level-2 heading test

--- tests/md2html.test.ts
+++ tests/md2html.test.ts
@@ -6,4 +6,8 @@ describe("Markdown level to HTML element", () => {
   it("converts a level-1 heading to <h1> [R-H-01]", () => {
     expect(md2html("# Hello")).toBe("<h1>Hello</h1>");
   });
+
+  it("converts a level-2 heading to <h2> [R-H-01]", () => {
+    expect(md2html("## Hello")).toBe("<h2>Hello</h2>");
+  });
 });
